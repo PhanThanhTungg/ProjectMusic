7a160124e80e5639075d7a2c28608bc9619c7774
git of tung <phantungpt03@gmail.com>
2025-03-08 15:22:44 +0700

    config cors

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
-import express,{ Express } from "express"
-const app:Express = express()
+import express,{ Express } from "express";
+const app:Express = express();
+
+import cors from "cors";
+app.use(cors());
 
 import dotenv from "dotenv";
 dotenv.config();
 
-import bodyParser from 'body-parser';
+import bodyParser from 'body-parser'; 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
