b9f8e98000e167edbd5589054fb4d6919f2cbf94
PhanThanhTungg <166277205+PhanThanhTungg@users.noreply.github.com>
2025-09-18 23:41:24 +0700

    fix deploy

--- helper/httpOnly.helper.ts
+++ helper/httpOnly.helper.ts
@@ -4,7 +4,7 @@ export const saveCookie = (res:any, type:string, token:string):void => {
   res.cookie(type, token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: +process.env.COOKIE_HTTP_ONLY_EXPIRE * 24 * 60 * 60 * 1000,
     path: "/"
   });
