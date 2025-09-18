ff6371db4d95996f488a3fa399338a070bbdcffa
PhanThanhTungg <166277205+PhanThanhTungg@users.noreply.github.com>
2025-09-18 23:21:52 +0700

    update logic

--- helper/httpOnly.helper.ts
+++ helper/httpOnly.helper.ts
@@ -3,8 +3,7 @@ import path from "path";
 export const saveCookie = (res:any, type:string, token:string):void => {
   res.cookie(type, token, {
     httpOnly: true,
-    // secure: process.env.NODE_ENV === "production",
-    secure: false,
+    secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
     maxAge: +process.env.COOKIE_HTTP_ONLY_EXPIRE * 24 * 60 * 60 * 1000,
     path: "/"
